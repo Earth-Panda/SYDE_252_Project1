7fca012a584ff5dec75e63effa011f1833e35591
Jason Heng <jasonjheng@gmail.com>
2022-11-03 20:51:48 -0400

    final touches on plotting t

--- main.m
+++ main.m
@@ -8,8 +8,10 @@ clc;
 %ProcessAudio("Speech.wav");
 
 plotAudio(Birds_Data,"Birds.wav", 2, 2, 1);
-output_data = Mean_Filter(Birds_Data, 3);
+%Best mean value for the filter is 13
+output_data = Mean_Filter(Birds_Data, 13);
 plotAudio(output_data,"Birds.wav", 2, 2, 2);
 
-plotAudioDiff(Birds_Data, "Mean","Birds.wav", 2,2,[3,4], 1:1:20);
+plotAudioDiff(Birds_Data, "Mean","Birds.wav", 2,2,[3,4], 1:1:30);
+%plotAudioDiff(Birds_Data, "WAvg","Birds.wav", 2,2,[3,4], 1:1:30);
 sound(output_data, Birds_Sample);
